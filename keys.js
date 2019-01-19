2f7de0c17dc9673f3fa6e2d4b9da6e69e708b615
Steve Shannon <you@example.com>
2019-01-19 17:09:18 -0500

    this is rage

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "S9MvMphN0ElAjKWXFQVbkpX3B",
+  consumer_secret: "LWp9cUqjS4KyWChjOlNf3QMT3pwTJHjJR5SiIl0ppte7bctZxG",
+  access_token_key: "22040865-3ihyBYip8f5tYmCODXxgtwnF2g2DsufmslPnpigKn",
+  access_token_secret: "yhHF96mTv1MbN7qUFbhR4mIi4FacZMncnkFFOgF5BX6KV",
+}
---
+++ liri.js
@@ -0,0 +1,3 @@
+// // i'm currently at the point where i have destroyed everything out of rage
+// my api keys consisitently don't work
+// have a great mlk weekend
