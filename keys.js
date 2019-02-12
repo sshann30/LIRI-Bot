3c20e8cfab8e2a0e5c96673b10994e5f5fc8a315
Steve Shannon <you@example.com>
2019-02-11 20:51:51 -0500

    work with tutor

---
+++ keys.js
@@ -0,0 +1,17 @@
+//   // i don't know how to use git ignore
+//   // I UNDERTAND WHY THERE ARE 5 RED X in console problems
+  
+//   exports.tmdbKey = '';
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
+  exports.omdb = {
+    key: process.env.OMDB_KEY
+  }
+  
+  exports.bit = {
+    key: process.env.APP_ID
+  }
--- liri.js
+++ liri.js
@@ -1,132 +1,155 @@
-
-//Loading modules
-// var Twitter = twitter api
-// var spotify = spotify api
-
-var fs = require('fs');
-
-var inputCommand = process.argv[2];
-var commandParam = process.argv[3];
-var defaultMovie = "Shrek";
-var defaultSong = "Numb";
-
-// figure out how to get a twitter API in node with out authentication
-// 
-// 
-// 
-// 
-
-// figure out how to get a spotify API in node with out authentication
-// 
-// 
-// 
-// 
+// MANY OF THE COMMENTED OUT SECTIONS ARE OLD CODE 
+// THAT I HAVE SINCE COMMENTED OUT AND REWRITEN BUT KEPT TO SEE WHAT IDEAS I NEEDED TO REPLACE
 
 
+// variables and requires
+require("dotenv").config();
+var fs = require("fs")
+var keys = require("./keys.js");
+var axios = require("axios")
+// const moment = require("moment");
 
-//All functions 
+// making a sentence but with code. Remember what morgan said about you have to -1. NODE + 1st COmmand + 2ND COmmand
 
-//Run commands and note defaults
-function processCommands(command, commandParam){
+var command = process.argv[2];
+var content = process.argv[3];
 
-	switch(command){
+var undef = (undef === 'undefined') ? def_val : undef;
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+});
+var omdb = keys.omdb.key;
+var bit = keys.bit.key;
 
-	case 'my-tweets':
-		getMyTweets();
+//
+if (command === 'concert-this') {
+    axios.get("https://rest.bandsintown.com/artists/" + content + "/events?app_id=" + bit + "")
+        .then(function (response) {
+            console.log("\nNext concert: " + response.data[0].venue.name);
+            console.log("Location: " + response.data[0].venue.city);
+            console.log("Time: " + response.data[0].datetime + "\n");
+        });
+}
+else if (command === 'spotify-this-song') {
+    spotify.search({ type: 'track', query: content }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // Artist(s)
+        console.log("Artists: " , data.tracks.items[0].album.artists[0].name);
+        // The song's name
+        console.log(data.tracks.items[0].name);
+        // A preview link of the song from Spotify
+        console.log(data.tracks.items[0].preview_url);
+        // The album that the song is from 
+        console.log(data.tracks.items[0].album.name);
 
-	case 'spotify-this-song':
-		//default is numb
-		if(commandParam === undefined){
-			commandParam = defaultSong;
-		}     
-		spotifyThis(commandParam);
+    })
+}
 
-	case 'movie-this':
-		//default is shrek
-		if(commandParam === undefined){
-			commandParam = defaultMovie;
-		}    
-		movieThis(commandParam);
+else if (command === 'movie-this') {
+    axios.get("http://www.omdbapi.com/?t=" + content + "&y=&plot=short&apikey=" + omdb)
+        .then(function (response) {
+            // OMDB API INFO where i can console log more info from the API if i wanted
 
-	case 'do-what-it-says':
-		doWhatItSays();
-
-	default: 
-		console.log("LIST OF USEABLE COMMANDS PROCEEDING 'node liri.js': 'my-tweets, spotify-this-song, movie-this, and do-what-it-says' ");
-};
-};
-
-// Twitter
-// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-var axios = require("axios");
-
-// Grab the movieName which will always be the third node argument.
-var movieName = process.argv[2];
-
-// Then run a request with axios to the OMDB API with the movie specified
-// whenever the hell the twitter API situation is figured out, put the query here
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-axios.get(queryUrl).then(
-  function(response) {
-	console.log("Title: " + response.data.Title);
-	console.log("Release Year: " + response.data.Year);
-	console.log("Artist: " + response.data.Artist);
-	console.log("Rating: " + response.data.Rating);
-  }
-);
-
-
-
-
-// Spotify
-// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-var axios = require("axios");
-
-// Grab the movieName which will always be the third node argument.
-var movieName = process.argv[2];
-
-// Then run a request with axios to the OMDB API with the movie specified
-// whenever the hell the spotify API situation is figured out, put the query here
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-axios.get(queryUrl).then(
-  function(response) {
-	console.log("Title: " + response.data.Title);
-	console.log("Release Year: " + response.data.Year);
-	console.log("Artist: " + response.data.Artist);
-	console.log("Rating: " + response.data.Rating);
-  }
-);
+            console.log("\nTitle: " + response.data.Title);
+            console.log("-_______________________________________-")
+            console.log("Year: " + response.data.Year);
+            console.log("-_______________________________________-")
+            console.log("Actors: " + response.data.Actors + "\n");
+            console.log("-_______________________________________-")
+            console.log("Country: " + response.data.Country);
+            console.log("-_______________________________________-")
+            console.log("Language: " + response.data.Language);
+            console.log("-_______________________________________-")
+            console.log("Plot: " + response.data.Plot);
+            console.log("-_______________________________________-")
+            console.log("IMDB Rating: " + response.data.Ratings[1].Value);
+            console.log("-_______________________________________-")
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[2].Value);
+            //rating preference?
+            console.log("-_______________________________________-")
+            JSON.stringify(response.data);
+            console.log("-_______________________________________-")
+        })
+}
 
 
+// 4
+// case "do-what-it-says":
+// doWhat();
+// break;
+
+// //(userCommand) for switching api commands 
+// mySwitch(userCommand)
+
+else if (command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var output = data.split(",");
+        var doitcontent = output[1];
+        // Spotify API INFO where i can console log more info from the API if i wanted
+        spotify.search({ type: "track", query: "" + doitcontent + "", limit: "1" })
+            .then(function (response) {
+                console.log("\nArtist: " + response.tracks.items[0].album.artists[0].name);
+                console.log("-_______________________________________-")
+                console.log("Song Name: " + response.tracks.items[0].name);
+                console.log("-_______________________________________-")
+                console.log("Album: " + response.tracks.items[0].album.name + "\n");
+                console.log("-_______________________________________-")
+                console.log("Album link: " + response.tracks.items[0].album.external_urls.spotify);
+                console.log("-_______________________________________-")
+            })
+            .catch(function (err) {
+                console.error('Sorry, no information found', err);
+            })
+    });
+}
+else {
+    console.log('invalid command')
+}
 
 
-// Movies
-// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
-var axios = require("axios");
+//         // DONE WITH API STUF. NOW how to switch!
+//         //('________________________________________');
+//         //How to siwtch from one api to another (listed above)
+//         function mySwitch(userCommand) {
+//             //options for (userCommand)
 
-// Grab the movieName which will always be the third node argument.
-var movieName = process.argv[2];
 
-// Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+//             switch (userCommand) {
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+//                 //1
+//                 case "my-tweets":
+//                     getTweets();
+//                     break;
+//                 // ("-_______________________________________-")
+//                 //2
+//                 case "spotify-this-song":
+//                     getSpotify();
+//                     break;
+//                 // ("-_______________________________________-")
 
-axios.get(queryUrl).then(
-  function(response) {
-	console.log("Title: " + response.data.Title);
-	console.log("Release Year: " + response.data.Year);
-	console.log("Actors: " + response.data.Actors);
-	console.log("Rating: " + response.data.Rating);
-  }
-);
+//                 //3
+//                 case "movie-this":
+//                     getMovie();
+//                     break;
+//                 // ("-_______________________________________-")
 
-function doWhatItSays(){
-	console.log("LIST OF USEABLE COMMANDS PROCEEDING 'node liri.js': 'my-tweets', 'spotify-this-song', 'movie-this', 'and do-what-it-says' ");}
+//                 //4
+//                 case "do-what-it-says":
+//                     doWhat();
+//                     break;
+
+//                     // ""
+//                     // ''
+//                     // }}};
+
+//                     //(userCommand) for switching api commands 
+//                     mySwitch(userCommand)
+//             }
+//         };
